194d71633de0905c5f70017af8e65988cd8ad058
Franz Andel <franz.andel@amarbank.co.id>
2021-08-14 15:42:55 +0700

    test(dashboard-vm-test): Create Unit Test

---
+++ app/src/test/java/com/franzandel/moviesubmission/utils/RetrofitUtils.kt
@@ -0,0 +1,11 @@
+package com.franzandel.moviesubmission.utils
+
+/**
+ * Created by Franz Andel on 14/08/21.
+ * Android Engineer
+ */
+
+object RetrofitUtils {
+
+    const val GET_DATA_FAILED = "Get data from server failed"
+}
