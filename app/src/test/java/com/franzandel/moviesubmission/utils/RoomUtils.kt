321a125bbc2e2880394297e4de32cb056648f14f
Franz Andel <franz.andel@amarbank.co.id>
2021-08-14 19:04:03 +0700

    test(movies-local-data-impl-test): Create Unit Test

--- app/src/test/java/com/franzandel/moviesubmission/utils/RoomUtils.kt
+++ app/src/test/java/com/franzandel/moviesubmission/utils/RoomUtils.kt
@@ -1,5 +1,6 @@
 package com.franzandel.moviesubmission.utils
 
+import com.franzandel.moviesubmission.data.local.entity.FavouriteMovieEntity
 import com.franzandel.moviesubmission.domain.model.FavouriteMovieReq
 import com.franzandel.moviesubmission.domain.model.FavouriteMovieRes
 import com.franzandel.moviesubmission.presentation.favouritemovies.model.FavouriteMovieResUI
@@ -76,6 +77,24 @@ object RoomUtils {
 
     fun getFavouriteMovieReq(): FavouriteMovieReq =
         FavouriteMovieReq(
+            id = 1,
+            adult = false,
+            backdropPath = "/iopYFB1b6Bh7FWZh3onQhph1sih.jpg",
+            genres = "Action, Comedy, Adventure",
+            originalLanguage = "en",
+            originalTitle = "Godzilla vs. Kong",
+            overview = "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
+            popularity = 9043.741,
+            posterPath = "/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg",
+            releaseDate = "2021-03-24",
+            title = "Godzilla vs. Kong",
+            video = false,
+            voteAverage = 7.1,
+            voteCount = 155
+        )
+
+    fun getFavouriteMovieEntity(): FavouriteMovieEntity =
+        FavouriteMovieEntity(
             id = 1,
             adult = false,
             backdropPath = "/iopYFB1b6Bh7FWZh3onQhph1sih.jpg",
@@ -239,6 +258,42 @@ object RoomUtils {
                 voteAverage = 7.1,
                 voteCount = 1292,
                 isFavourite = true
+            )
+        )
+
+    fun getFavouriteMoviesEntity(): List<FavouriteMovieEntity> =
+        listOf(
+            FavouriteMovieEntity(
+                id = 1,
+                adult = false,
+                backdropPath = "/iopYFB1b6Bh7FWZh3onQhph1sih.jpg",
+                genres = "Action, Comedy, Adventure",
+                originalLanguage = "en",
+                originalTitle = "Godzilla vs. Kong",
+                overview = "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
+                popularity = 9043.741,
+                posterPath = "/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg",
+                releaseDate = "2021-03-24",
+                title = "Godzilla vs. Kong",
+                video = false,
+                voteAverage = 7.1,
+                voteCount = 155
+            ),
+            FavouriteMovieEntity(
+                id = 2,
+                adult = false,
+                backdropPath = "/z8TvnEVRenMSTemxYZwLGqFofgF.jpg",
+                genres = "Action, Comedy, Adventure",
+                originalLanguage = "en",
+                originalTitle = "Monster Hunter",
+                overview = "A portal transports Cpt. Artemis and an elite unit of soldiers to a strange world where powerful monsters rule with deadly ferocity. Faced with relentless danger, the team encounters a mysterious hunter who may be their only hope to find a way home.",
+                popularity = 2021.063,
+                posterPath = "/1UCOF11QCw8kcqvce8LKOO6pimh.jpg",
+                releaseDate = "2020-12-03",
+                title = "Monster Hunter",
+                video = false,
+                voteAverage = 7.1,
+                voteCount = 1292
             )
         )
 }
