236e6da7aa45766990479ae700537db903a02b30
Franz Andel <franz.andel@amarbank.co.id>
2021-08-14 13:31:43 +0700

    test(detail-favourite-movie-vm-test): Create Unit Test

--- app/src/test/java/com/franzandel/moviesubmission/utils/RoomUtils.kt
+++ app/src/test/java/com/franzandel/moviesubmission/utils/RoomUtils.kt
@@ -1,6 +1,7 @@
 package com.franzandel.moviesubmission.utils
 
 import com.franzandel.moviesubmission.domain.model.FavouriteMovieReq
+import com.franzandel.moviesubmission.presentation.favouritemovies.model.FavouriteMovieResUI
 import com.franzandel.moviesubmission.presentation.popularmovies.model.PopularMovieResUI
 import com.franzandel.moviesubmission.presentation.topratedmovies.model.TopRatedMovieResUI
 
@@ -36,6 +37,25 @@ object RoomUtils {
 
     fun getTopRatedMovieResUI(): TopRatedMovieResUI =
         TopRatedMovieResUI(
+            id = 399566,
+            adult = false,
+            backdropPath = "/iopYFB1b6Bh7FWZh3onQhph1sih.jpg",
+            genres = "Action, Comedy, Adventure",
+            originalLanguage = "en",
+            originalTitle = "Godzilla vs. Kong",
+            overview = "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
+            popularity = 9043.741,
+            posterPath = "/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg",
+            releaseDate = "2021-03-24",
+            title = "Godzilla vs. Kong",
+            video = false,
+            voteAverage = 7.1,
+            voteCount = 155,
+            isFavourite = true
+        )
+
+    fun getFavouriteMovieResUI(): FavouriteMovieResUI =
+        FavouriteMovieResUI(
             id = 399566,
             adult = false,
             backdropPath = "/iopYFB1b6Bh7FWZh3onQhph1sih.jpg",
