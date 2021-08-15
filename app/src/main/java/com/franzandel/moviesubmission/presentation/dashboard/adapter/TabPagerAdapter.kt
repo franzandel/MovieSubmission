f06ee652d98208352d61dd413e25680b811628ea
Franz Andel <franz.andel@amarbank.co.id>
2021-08-15 19:58:53 +0700

    refactor(dashboard-activity): Refactor viewpager to viewpager2

--- app/src/main/java/com/franzandel/moviesubmission/presentation/dashboard/adapter/TabPagerAdapter.kt
+++ app/src/main/java/com/franzandel/moviesubmission/presentation/dashboard/adapter/TabPagerAdapter.kt
@@ -1,36 +1,20 @@
 package com.franzandel.moviesubmission.presentation.dashboard.adapter
 
-import android.content.Context
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import com.franzandel.moviesubmission.R
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.franzandel.moviesubmission.presentation.favouritemovies.fragment.FavouriteMoviesFragment
 import com.franzandel.moviesubmission.presentation.popularmovies.fragment.PopularMoviesFragment
 import com.franzandel.moviesubmission.presentation.topratedmovies.fragment.TopRatedMoviesFragment
 
-/**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
- * one of the sections/tabs/pages.
- */
-class TabPagerAdapter(private val context: Context, fm: FragmentManager) :
-    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class TabPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
 
-    private val tabTitles = arrayOf(
-        R.string.popular_movies_tab_title,
-        R.string.top_rated_movies_tab_title,
-        R.string.favourite_movies_tab_title
-    )
+    override fun getItemCount(): Int = 3
 
-    override fun getItem(position: Int): Fragment =
+    override fun createFragment(position: Int): Fragment =
         when (position) {
             0 -> PopularMoviesFragment()
             1 -> TopRatedMoviesFragment()
             else -> FavouriteMoviesFragment()
         }
-
-    override fun getPageTitle(position: Int): CharSequence =
-        context.resources.getString(tabTitles[position])
-
-    override fun getCount(): Int = tabTitles.size
 }
