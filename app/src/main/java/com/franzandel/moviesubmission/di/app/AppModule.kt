4db9a79d6c6d5244bec5e9856f99bb0f5275a620
Franz Andel <franz.andel@amarbank.co.id>
2021-08-15 15:53:51 +0700

    feat(app-module): Test removing certificate pinner

--- app/src/main/java/com/franzandel/moviesubmission/di/app/AppModule.kt
+++ app/src/main/java/com/franzandel/moviesubmission/di/app/AppModule.kt
@@ -1,7 +1,6 @@
 package com.franzandel.moviesubmission.di.app
 
 import android.content.Context
-import android.net.Uri
 import androidx.room.Room
 import com.franzandel.moviesubmission.BuildConfig
 import com.franzandel.moviesubmission.data.local.database.FavouriteMoviesDatabase
@@ -11,7 +10,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -29,20 +27,20 @@ object AppModule {
 
     private const val MOVIES_DB_NAME = "Movies.db"
     private const val TIMEOUT_TIME = 60L
-    private val domainUrl = Uri.parse(BuildConfig.MOVIE_DB_BASE_URL).host.toString()
+//    private val domainUrl = Uri.parse(BuildConfig.MOVIE_DB_BASE_URL).host.toString()
 
-    private val certificatePinner = CertificatePinner.Builder()
-        .add(domainUrl, BuildConfig.CERT_PINNER_1)
-        .add(domainUrl, BuildConfig.CERT_PINNER_2)
-        .add(domainUrl, BuildConfig.CERT_PINNER_3)
-        .add(domainUrl, BuildConfig.CERT_PINNER_4)
-        .build()
+//    private val certificatePinner = CertificatePinner.Builder()
+//        .add(domainUrl, BuildConfig.CERT_PINNER_1)
+//        .add(domainUrl, BuildConfig.CERT_PINNER_2)
+//        .add(domainUrl, BuildConfig.CERT_PINNER_3)
+//        .add(domainUrl, BuildConfig.CERT_PINNER_4)
+//        .build()
 
     @Provides
     @Singleton
     fun provideOkHttpClient(): OkHttpClient =
         OkHttpClient.Builder()
-            .certificatePinner(certificatePinner)
+//            .certificatePinner(certificatePinner)
             .writeTimeout(TIMEOUT_TIME, TimeUnit.SECONDS)
             .readTimeout(TIMEOUT_TIME, TimeUnit.SECONDS)
             .connectTimeout(TIMEOUT_TIME, TimeUnit.SECONDS)
